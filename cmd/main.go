76fb47b5b232a670c43e8e29ac80f2f61c9e0089
junskeep <junskee@kuberix.com>
2023-12-04 18:07:53 +0900

    helm index

--- cmd/main.go
+++ cmd/main.go
@@ -69,7 +69,7 @@ func main() {
 		Metrics:                metricsserver.Options{BindAddress: metricsAddr},
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "a6bee935.test.kubebuilder.io",
+		LeaderElectionID:       "a6bee935.core.kubegpt.io",
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
